2b18151b4209f526614d1664b6c8291c22e14113
Justin Collum <Justin.Collum@cdk.com>
2016-12-04 14:46:14 -0800

    fixed failing test for android version of app

--- index.android.js
+++ index.android.js
@@ -4,59 +4,68 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class kisharNine extends Component {
-  _incremement() {
-      this.setState({count : ++this.state.count})
-  }
-
-  render() {
-      return (
-          <View style={styles.container}>
-              <Text style={[
-                  styles.welcome, {
-                      marginBottom: 40
-                  }
-              ]}>
-                  Welcome to Kishar Nine
-              </Text>
-              <Text style={styles.instructions}>
-                  Simple Counter Example
-              </Text>
-              <Text style={styles.heading}>{this.state.count}</Text>
-              <TouchableHighlight underlayColor='lightskyblue' style={{margin: 40}} activeOpacity={20} onPress={() => {
-                  this._incremement()
-              }}><Text style={[styles.heading, {color: 'blue', marginTop: 0}]}>+</Text></TouchableHighlight>
-
-          </View>
-      );
-  }
-}
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View, TouchableHighlight} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    heading: {
+        fontSize: 30,
+        marginTop: 10,
+        color: '#000',
+        fontWeight: 'bold'
+    },
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    }
 });
+
+export default class kisharNine extends Component {
+    constructor(props) {
+        super(props);
+        this.state = ({count: 1});
+
+    }
+
+    _incremement() {
+        this.setState({count : ++this.state.count})
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={[
+                    styles.welcome, {
+                        marginBottom: 40
+                    }
+                ]}>
+                    Welcome to Kishar Nine
+                </Text>
+                <Text style={styles.instructions}>
+                    Simple Counter Example
+                </Text>
+                <Text style={styles.heading}>{this.state.count}</Text>
+                <TouchableHighlight underlayColor='lightskyblue' style={{margin: 40}} activeOpacity={20} onPress={() => {
+                    this._incremement()
+                }}><Text style={[styles.heading, {color: 'blue', marginTop: 0}]}>+</Text></TouchableHighlight>
+
+            </View>
+        );
+    }
+}
+
 
 AppRegistry.registerComponent('kisharNine', () => kisharNine);
