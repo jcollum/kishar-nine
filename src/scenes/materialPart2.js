1f7f344bea4748c1b0ddbe15439cb1b7a1312c08
Collum, Justin <Justin.Collum@cdk.com>
2017-01-02 08:54:29 -0800

    some changes to the text boxes

--- src/scenes/materialPart2.js
+++ src/scenes/materialPart2.js
@@ -1,15 +1,22 @@
 import React, {Component, PropTypes} from 'react';
 import {View, Text, TouchableHighlight, Linking} from 'react-native';
-import {styles} from '../styles'
+//import {styles} from '../styles'
 import {Actions} from 'react-native-router-flux';
 import {RaisedButton} from './../components/raisedButton';
-styles.textfield = {
-    height: 48, // have to do it on iOS
-    marginTop: 60
-}
-styles.textfieldWithFloatingLabel = {
-    height: 48, // have to do it on iOS
-    marginTop: 10
+styles = {
+    textfield: {
+        height: 36, // have to do it on iOS
+        marginTop: 10
+    },
+    textfieldWithFloatingLabel: {
+        height: 36, // have to do it on iOS
+        marginTop: 10
+    },
+    textView: {
+        backgroundColor: "#DDD",
+        margin: 5,
+        padding: 10
+    }
 }
 import {
     mdl,
@@ -20,9 +27,7 @@ import {
     TickView
 } from 'react-native-material-kit';
 
-const TextfieldWithFloatingLabel = MKTextField.textfieldWithFloatingLabel().withPlaceholder('Number...')
-.withStyle(styles.textfieldWithFloatingLabel).withTextInputStyle({flex: 1})
-.withFloatingLabelFont({fontSize: 10, fontStyle: 'italic', fontWeight: '200'}).withKeyboardType('numeric').build();
+const TextfieldWithFloatingLabel = MKTextField.textfieldWithFloatingLabel().withPlaceholder('Type a number...').withStyle(styles.textfieldWithFloatingLabel).withTextInputStyle({flex: 1}).withFloatingLabelFont({fontSize: 10, fontStyle: 'italic', fontWeight: '200'}).withKeyboardType('numeric').build();
 
 export default class MaterialPart2 extends Component {
     constructor(props) {
@@ -31,22 +36,28 @@ export default class MaterialPart2 extends Component {
     render() {
         return (
             <View>
-                <View style={{
-                    margin: 20,
-                    padding: 20,
-                    backgroundColor: "#CCC"
-                }}>
-                    <MKTextField tintColor={MKColor.Orange} textInputStyle={{
+                {/* this textbox is messed up in this version of RNMK?  */}
+                <View style={[
+                    styles.textView, {
+                        marginTop: 20
+                    }
+                ]}>
+                    <MKTextField textInputStyle={{
+                        flex: 1
+                    }} keyboardType="ascii-capable" tintColor={MKColor.Orange} textInputStyle={{
                         color: MKColor.Blue
                     }} placeholder="Text…" style={styles.textfield}/>
+                    <Text style={{
+                        fontSize: 12
+                    }}>(This text box might be broken in RNMK 0.3.x)</Text>
                 </View>
-                <View style={{
-                    margin: 20,
-                    padding: 20,
-                    backgroundColor: "#CCC"
-                }}>
+                <View style={styles.textView}>
                     <TextfieldWithFloatingLabel ref="defaultInput"/>
-                    <Text style={styles.legendLabel}>With floating label</Text>
+                    <Text style={[
+                        styles.legendLabel, {
+                            marginTop: 5
+                        }
+                    ]}>Number of things</Text>
                 </View>
             </View>
         )
